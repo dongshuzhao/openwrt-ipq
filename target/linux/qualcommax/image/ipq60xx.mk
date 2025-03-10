d68511accae25f5fdaaf887d305cd8acb8d70ce7
dongshuzhao <dongshuzhao@users.noreply.github.com>
2025-03-10 15:09:29 +0000

    Disable axt1800 for missing pwm error

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -61,19 +61,19 @@ define Device/glinet_gl-ax1800
 endef
 TARGET_DEVICES += glinet_gl-ax1800
 
-define Device/glinet_gl-axt1800
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := GL-iNet
-	DEVICE_MODEL := GL-AXT1800
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@cp03-c1
-	SOC := ipq6018
-	DEVICE_PACKAGES := ipq-wifi-glinet_gl-axt1800 kmod-hwmon-core e2fsprogs dosfstools kmod-fs-ext4 kmod-fs-ntfs kmod-fs-vfat \
-		kmod-fs-exfat kmod-hwmon-pwmfan block-mount kmod-usb-storage kmod-usb2 fdisk
-endef
-TARGET_DEVICES += glinet_gl-axt1800
+#define Device/glinet_gl-axt1800
+#	$(call Device/FitImage)
+#	$(call Device/UbiFit)
+#	DEVICE_VENDOR := GL-iNet
+#	DEVICE_MODEL := GL-AXT1800
+#	BLOCKSIZE := 128k
+#	PAGESIZE := 2048
+#	DEVICE_DTS_CONFIG := config@cp03-c1
+#	SOC := ipq6018
+#	DEVICE_PACKAGES := ipq-wifi-glinet_gl-axt1800 kmod-hwmon-core e2fsprogs dosfstools kmod-fs-ext4 kmod-fs-ntfs kmod-fs-vfat \
+#		kmod-fs-exfat kmod-hwmon-pwmfan block-mount kmod-usb-storage kmod-usb2 fdisk
+#endef
+#TARGET_DEVICES += glinet_gl-axt1800
 
 define Device/jdc_ax1800-pro
 	$(call Device/FitImage)
--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -135,7 +135,6 @@ platform_do_upgrade() {
 	qihoo,v6|\
 	redmi,ax5|\
 	xiaomi,ax1800|\
-	glinet,gl-axt1800|\
 	glinet,gl-ax1800|\
 	netgear,wax214)
 		nand_do_upgrade "$1"
